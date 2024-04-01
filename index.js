97000470932e222ed2a2d77d2270f6563feed36f
RashedAbdullah <maarashed40@gmail.com>
2024-04-02 00:32:06 +0600

    node.js learing

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log(__dirname);
+// console.log(__filename);
+
+// var a = 5;
+
+// console.log(module);
+
+const names = require("./index2");
+
+console.log(names);
---
+++ index2.js
@@ -0,0 +1,6 @@
+// var b = 2;
+// console.log(a + b);
+
+const peoples = ["Rashed", "Labib", "Ismat"];
+
+module.exports = peoples;
