8127915c3561ff9ca35f213bfd5b4dbf833f4a9f
RashedAbdullah <maarashed40@gmail.com>
2024-04-02 13:00:42 +0600

    os files

--- modules/os.js
+++ modules/os.js
@@ -1,6 +1,18 @@
-// const os = require("os"); //Operating System:
-// console.log(os.platform())
-// console.log(os.homedir())
-// console.log(os.hostname())
-// console.log(os.freemem())
-// console.log(os.cpus())
+const os = require("os"); //Operating System:
+// console.log(os.platform()) //linux
+// console.log(os.homedir()) ///home/rashedabdullah
+// console.log(os.hostname()) //rashed-Modern-15
+// console.log(os.freemem()) //13834604544
+// console.log(os.totalmem()) //16083931136
+// console.log(os.cpus());
+// console.log(os.userInfo())
+// console.log(os.machine()) //x86_64
+// console.log(os.networkInterfaces())
+// console.log(os.type()) //linux
+// console.log(os.version()) //#26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2
+// console.log(os.getPriority()) //0
+// console.log(os.arch()) //x64
+// console.log(os.availableParallelism()) //12
+// console.log(os.endianness()) //LE
+// console.log(os.loadavg()) //[ 0.39, 0.47, 0.39 ]
+// console.log(os.release()) //6.5.0-26-generic
