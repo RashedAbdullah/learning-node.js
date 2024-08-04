b94fb85a08d0de089686215f023cad97a1a0f771
RashedAbdullah <maarashed40@gmail.com>
2024-08-04 23:03:07 +0600

    Some nodejs besics

--- projects/project01/index.js
+++ projects/project01/index.js
@@ -2,4 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  console.log(req.baseUrl);
+  res.end("Rashed Abdullah");
+});
+
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
