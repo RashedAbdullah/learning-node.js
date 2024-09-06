20e7f54fbf722f08c33d796b36956c15f1e8a1d7
RashedAbdullah <maarashed40@gmail.com>
2024-09-06 15:39:38 +0600

    project

--- projects/project01/index.js
+++ projects/project01/index.js
@@ -2,9 +2,41 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const users = require("./users.json");
+
 app.get("/", (req, res) => {
   console.log(req.baseUrl);
   res.end("Rashed Abdullah");
 });
 
-app.listen(PORT, () => console.log(`Server started at ${PORT}`));
+// JSON Data
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+// HTML Data
+app.get("/api/users/html", (req, res) => {
+  const html = `
+  <ul>
+  ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+  </ul>
+  `;
+  res.send(html);
+});
+
+// Single user / Dynamic path parameters
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .post((req, res) => {})
+  .patch((req, res) => {
+    // Edit:
+    return res.json({ status: "Panding..." });
+  })
+  .delete((req, res) => {});
+
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
