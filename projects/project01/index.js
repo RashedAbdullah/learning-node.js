75ec603e800cb4be06e1a5d8b841ec2ef2f08028
RashedAbdullah <maarashed40@gmail.com>
2024-09-08 12:35:54 +0600

    Nodejs with MongoDB

--- projects/project01/index.js
+++ projects/project01/index.js
@@ -1,35 +1,86 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const fs = require("fs");
 const app = express();
 const PORT = 3000;
-
 const users = require("./users.json");
+
+// Connection with mongoDB:
+mongoose
+  .connect("mongodb://localhost:27017/jamiatul_latif")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Schema:
+const texamSchema = new mongoose.Schema(
+  {
+    examName: {
+      type: String,
+      required: true,
+    },
+    examYear: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Model:
+const examModel = mongoose.model("Exam", texamSchema);
 
 app.get("/", (req, res) => {
   console.log(req.baseUrl);
   res.end("Rashed Abdullah");
 });
 
-// JSON Data
-app.get("/api/users", (req, res) => {
-  return res.json(users);
-});
-
+// Middlware
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/api/users", (req, res) => {
+app.use((req, res, next) => {
+  // return res.json({ message: "Response from middlware two" });
+  req.myCity = "Feni Bangladesh";
+  next();
+});
+
+app.use((req, res, next) => {
+  // console.log(req.myCity);
+  // return res.json({ message: "Response from middlware three" });
+  next();
+});
+
+// JSON Data
+app.get("/api/users", async (req, res) => {
+  // console.log(req.headers.nephew);
+
+  // res.setHeader("myName", "Rashed Abdullah");
+  const exams = await examModel.find({});
+
+  // return res.json(users);
+  return res.json(exams);
+});
+
+app.post("/api/users", async (req, res) => {
   const body = req.body;
-  users.push(body);
-  fs.writeFile("./users.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "Successfully data inserted..." });
+  // users.push(body);
+  // fs.writeFile("./users.json", JSON.stringify(users), (err, data) => {
+  //   return res.status(201).json({ status: "Successfully data inserted..." });
+  // });
+
+  const exam = await examModel.create({
+    examName: body.examName,
+    examYear: body.examYear,
   });
+
+  return res.status(201).json({ msg: "Successfully created exam" });
 });
 
 // HTML Data
-app.get("/api/users/html", (req, res) => {
+app.get("/api/users/html", async (req, res) => {
+  const exams = await examModel.find({});
   const html = `
   <ul>
-  ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+  ${exams.map((exam) => `<li>${exam.examName}</li>`).join("")}
   </ul>
   `;
   res.send(html);
@@ -38,10 +89,11 @@ app.get("/api/users/html", (req, res) => {
 // Single user / Dynamic path parameters
 app
   .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
+  .get(async (req, res) => {
+    const exam = await examModel.findById(req.params.id);
+    // const id = Number(req.params.id);
+    // const user = users.find((user) => user.id === id);
+    return res.json(exam);
   })
   .patch((req, res) => {
     // Edit:
